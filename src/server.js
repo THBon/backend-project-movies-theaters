f047adb48fab6cb643bb22b44e77192a736db782
THBon <thbonc@gmail.com>
2022-01-25 04:11:32 -0500

    server changes

--- src/server.js
+++ src/server.js
@@ -1,26 +1,18 @@
-const PORT = process.env.PORT || 5000
-
-const app = require("./app");
-const knex = require("./db/connection");
-
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-
-knex.migrate
-  .latest()
-  .then((migrations) => {
-    console.log("migrations", migrations);
-    app.listen(PORT, listener);
-  })
-  .catch(console.error);
-
-
-const express = require("express")
-const app = express()
+const express = require('express')
 const cors = require('cors')
+
+const app = express()
 const router = express.Router()
+const PORT = process.env.PORT || 5000
 
 router.get("/", cors(), (req, res) => {
   res.json({ message: "Data can be access via the following routes: /movies, /movies/:movieId, /movies/:movieId/theaters, /movies/:movieId/reviews, /reviews, /reviews/:reviewId, and /theaters"});
 });
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `);
+})
 
 module.exports = app
